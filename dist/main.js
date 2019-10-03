4bb716661281c861253f87e1fc5c98999579b993
Kirilllert <46005727+Kirilllert@users.noreply.github.com>
2019-10-03 21:07:17 +0300

    summary

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,100 @@
-!function(t){var e={};function n(o){if(e[o])return e[o].exports;var i=e[o]={i:o,l:!1,exports:{}};return t[o].call(i.exports,i,i.exports,n),i.l=!0,i.exports}n.m=t,n.c=e,n.d=function(t,e,o){n.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:o})},n.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},n.t=function(t,e){if(1&e&&(t=n(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var i in t)n.d(o,i,function(e){return t[e]}.bind(null,i));return o},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},n.p="",n(n.s=0)}([function(t,e){let n=document.getElementById("inputexit");class o{constructor(t){this.context=t}setup(){this.oscillator=this.context.createOscillator(),this.gainNode=this.context.createGain(),this.oscillator.connect(this.gainNode),this.gainNode.connect(this.context.destination),this.oscillator.type="sine"}play(t){this.setup(),this.oscillator.frequency.value=t,this.gainNode.gain.setValueAtTime(0,this.context.currentTime),this.gainNode.gain.linearRampToValueAtTime(1,this.context.currentTime+.01),this.oscillator.start(this.context.currentTime),this.stop(this.context.currentTime)}stop(){this.gainNode.gain.exponentialRampToValueAtTime(.001,this.context.currentTime+1),this.oscillator.stop(this.context.currentTime+1)}}let i=new(window.AudioContext||window.webkitAudioContext);document.addEventListener("click",(function(t){}));document.querySelector(".stick"),document.querySelector(".stick .glow-1"),document.querySelector(".stick .glow-2");const r=document.querySelectorAll(".note");function c(){}r.forEach(t=>{t.addEventListener("click",()=>{!function(t){let e=new o(i),n=t.dataset.frequency;e.play(n),e.stop()}(t),setTimeout(c,300)}),t.addEventListener("click",c)}),r.forEach(t=>{t.addEventListener("click",()=>{n.value=t.textContent}),t.addEventListener("click",c)})}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("    let inputExit = document.getElementById('inputexit');\r\n\r\n    class Sound {\r\n\r\n        constructor(context) {\r\n            this.context = context;\r\n        }\r\n\r\n        setup() {\r\n            this.oscillator = this.context.createOscillator();\r\n            this.gainNode = this.context.createGain();\r\n\r\n            this.oscillator.connect(this.gainNode);\r\n            this.gainNode.connect(this.context.destination);\r\n            this.oscillator.type = 'sine';\r\n        }\r\n\r\n        play(value) {\r\n            this.setup();\r\n\r\n            this.oscillator.frequency.value = value;\r\n            this.gainNode.gain.setValueAtTime(0, this.context.currentTime);\r\n            this.gainNode.gain.linearRampToValueAtTime(1, this.context.currentTime + 0.01);\r\n\r\n            this.oscillator.start(this.context.currentTime);\r\n            this.stop(this.context.currentTime);\r\n        }\r\n\r\n        stop() {\r\n            this.gainNode.gain.exponentialRampToValueAtTime(0.001, this.context.currentTime + 1);\r\n            this.oscillator.stop(this.context.currentTime + 1);\r\n        }\r\n\r\n    }\r\n\r\n    let context = new(window.AudioContext || window.webkitAudioContext)();\r\n\r\n\r\n    document.addEventListener('click', cursor);\r\n    const stick = document.querySelector('.stick');\r\n    const glow1 = document.querySelector('.stick .glow-1');\r\n    const glow2 = document.querySelector('.stick .glow-2');\r\n    const notes = document.querySelectorAll('.note');\r\n\r\n    notes.forEach((note) => {\r\n        note.addEventListener('click', () => {\r\n            playSound(note);\r\n            showGlow();\r\n            setTimeout(hideGlow, 300);\r\n        })\r\n        note.addEventListener('click', hideGlow);\r\n    })\r\n\r\n    function playSound(note) {\r\n        let sound = new Sound(context);\r\n        let value = note.dataset.frequency;\r\n        sound.play(value);\r\n        sound.stop();\r\n    }\r\n\r\n    function showGlow() {\r\n\r\n    }\r\n\r\n    function hideGlow() {\r\n\r\n    }\r\n\r\n    function cursor(e) {\r\n\r\n    }\r\n\r\n    notes.forEach((note) => {\r\n        note.addEventListener('click', () => {\r\n            inputExit.value = note.textContent\r\n        })\r\n        note.addEventListener('click', hideGlow);\r\n    })\r\n\r\n    \n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ })
+
+/******/ });
---
+++ script.js
@@ -0,0 +1,82 @@
+    let inputExit = document.getElementById('inputexit');
+    let massivenotes = [];
+    let note = document.getElementById('notes');
+    const play = document.getElementById('play');
+    const save = document.getElementById('save');
+
+    class Sound {
+
+        constructor(context) {
+            this.context = context;
+        }
+
+        setup() {
+            this.oscillator = this.context.createOscillator();
+            this.gainNode = this.context.createGain();
+
+            this.oscillator.connect(this.gainNode);
+            this.gainNode.connect(this.context.destination);
+            this.oscillator.type = 'sine';
+        }
+
+        play(value) {
+            this.setup();
+
+            this.oscillator.frequency.value = value;
+            this.gainNode.gain.setValueAtTime(0, this.context.currentTime);
+            this.gainNode.gain.linearRampToValueAtTime(1, this.context.currentTime + 0.01);
+
+            this.oscillator.start(this.context.currentTime);
+            this.stop(this.context.currentTime);
+        }
+
+        stop() {
+            this.gainNode.gain.exponentialRampToValueAtTime(0.001, this.context.currentTime + 1);
+            this.oscillator.stop(this.context.currentTime + 1);
+        }
+
+    }
+
+    let context = new(window.AudioContext || window.webkitAudioContext)();
+
+
+    document.addEventListener('click', cursor);
+    const stick = document.querySelector('.stick');
+    const glow1 = document.querySelector('.stick .glow-1');
+    const glow2 = document.querySelector('.stick .glow-2');
+    const notes = document.querySelectorAll('.note');
+
+    notes.forEach((note) => {
+        note.addEventListener('click', () => {
+            playSound(note);
+            showGlow();
+            setTimeout(hideGlow, 300);
+        })
+        note.addEventListener('click', hideGlow);
+    })
+
+    function playSound(note) {
+        let sound = new Sound(context);
+        let value = note.dataset.frequency;
+        sound.play(value);
+        sound.stop();
+    }
+
+    function showGlow() {
+
+    }
+
+    function hideGlow() {
+
+    }
+
+    function cursor(e) {
+
+    }
+     for (let i = 0; i < notes.length; i++) {
+            notes[i].addEventListener('click', () =>{
+            inputExit.value += `${notes[i].textContent}`;
+            //inputExit.value = finilMassiveNotes;
+         })
+     }
+ 
